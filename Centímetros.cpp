38dcc5ddd0b5f38e7927d4337cf06923ab3b32ff
JULIO CESAR OLIVEIRA SIRIANO <juliocesarsiriano44@gmail.com>
2025-03-07 20:34:22 -0300

    Update Centímetros.cpp

--- Centímetros.cpp
+++ Centímetros.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main (){
 	
-	setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "pt_BR.UTF-8");
 	
 	double metros, centimetros;
 	
-	cout << "Digite quantos centímetros você deseja converter: \n";
+	cout << "Digite quantos centÃ­metros vocÃª deseja converter: \n";
 	cin >> centimetros;
 	
 	metros = centimetros * 0.01;
 	
-	cout << "\n" << centimetros <<" centímetros correspodem a " << metros << " metros.\n\n";
+	cout << "\n" << centimetros <<" centÃ­metros correspodem a " << metros << " metros.\n\n";
 	
 	system("pause");
 	return 0;
